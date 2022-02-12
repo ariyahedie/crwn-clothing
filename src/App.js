4dc2f4a71b5b9e97de6afd69dd57b623e1583182
Ariya Hedie Taniyara <ariya.taniyara@gdn-commerce.com>
2022-02-12 17:10:02 +0700

    update app and add collection item and collection preview to shop page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import HatsPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={HatsPage} />
       </Switch>
     </div>
   );
